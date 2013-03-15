a0f4476e0b59b105f6188863cafe8c2e77728660
Alex Soulim <soulim@gmail.com>
2013-03-15 10:31:33 +0400

    Fix email delivery method configuration

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.default_url_options = {
   :host => ENV['DEFAULT_URL_OPTIONS_HOST']
 }
-ActionMailer::Base.delivery_method = ENV['EMAIL_DELIVERY_METHOD']
+ActionMailer::Base.delivery_method = ENV['EMAIL_DELIVERY_METHOD'].to_sym
 ActionMailer::Base.smtp_settings = {
   :address => ENV['SMTP_ADDRESS'],
   :port => ENV['SMTP_PORT'],

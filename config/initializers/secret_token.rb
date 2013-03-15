56f191ce6a0aa6ade244ab427970eb8dca194155
Alex Soulim <soulim@gmail.com>
2013-03-15 09:10:01 +0400

    Hide session secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rbjobs::Application.config.secret_token = 'a291ae8412bd5d3088b2bd5e7d888f6d3d4002f38e8716f33014fad5e7975b4517fcd66b7e381cd7cad4e9f2147720f8a91a7fed43e4a95c6635d8b18eb84b33'
+Rbjobs::Application.config.secret_token = ENV['SESSION_SECRET'] || 'afake32charactertokenfortesting'

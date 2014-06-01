e33906f5b8abe52a9113f31e8da6822e8094ee1f
Alex Sulim <hello@sul.im>
2014-06-01 23:37:56 +0200

    Puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,5 @@
 bind 'unix:///tmp/puma.rubyjobs.sock'
+environment (ENV['RACK_ENV'] || 'development' )
 threads (ENV['PUMA_MIN_THREADS'] || 4 ), (ENV['PUMA_MAX_THREADS'] || 4 )
 workers (ENV['PUMA_WORKERS'] || 2 )
 preload_app!

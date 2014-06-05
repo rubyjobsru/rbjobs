13fa557e6af366d78ac5eb425b55603369d4c691
Alex Sulim <hello@sul.im>
2014-06-05 16:27:41 +0200

    Stop using deprecated method

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe Page do
   subject{ stub_model(Page) }
-  
+
   describe ".find_by_id" do
     context "when page exists" do
-      before{ Page.stub!(:exists? => true) }
-      
+      before{ Page.stub(:exists? => true) }
+
       it "should create a new page" do
         Page.should_receive(:new).with("foo")
         Page.find_by_id("foo")
@@ -17,8 +17,8 @@ describe Page do
       end
     end
     context "when page doesn't exists" do
-      before{ Page.stub!(:exists? => false) }
-      
+      before{ Page.stub(:exists? => false) }
+
       it "should return nil" do
         Page.find_by_id("foo").should be_nil
       end
@@ -26,8 +26,8 @@ describe Page do
   end
 
   describe ".exists?" do
-    before{ Page.stub!(:ids => ["foo", "bar"]) }
-    
+    before{ Page.stub(:ids => ["foo", "bar"]) }
+
     context "when list of ids includes given id" do
       it "should return true" do
         Page.exists?("foo").should be_true

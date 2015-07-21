195ec2262c2b58cb45d45a6866c0790abd83b83e
Franze Jr <franzejr@gmail.com>
2015-07-20 21:50:31 -0300

    Removing trailing whitespace

--- lib/html_generator.rb
+++ lib/html_generator.rb
@@ -2,12 +2,12 @@ module HtmlGenerator
   def self.render(source, strategy = Markdown.new)
     strategy.render(source)
   end
-  
+
   class Markdown
     @@renderer = Redcarpet::Render::HTML.new(:hard_wrap => true, :filter_html => true, :no_images => true,
                                              :no_links => false, :safe_links_only => true, :no_styles => true)
     @@markdown = Redcarpet::Markdown.new(@@renderer, :autolink => false)
-    
+
     def render(source)
       @@markdown.render(source)
     end

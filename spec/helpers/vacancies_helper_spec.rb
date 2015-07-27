248c1e2225f9420bae3b29efcf67c16972121d25
Alex Sulim <hello@sul.im>
2015-07-27 09:50:30 +0200

    Upgrade VacanciesHelper to RSpec 3

--- spec/helpers/vacancies_helper_spec.rb
+++ spec/helpers/vacancies_helper_spec.rb
@@ -1,36 +1,54 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe VacanciesHelper do
-  let(:vacancy){ stub_model(Vacancy, :company => "Foo Inc.", :location => "Neverland") }
-  
-  describe "company_location_tag" do
-    context "when company and location are presented" do
-      it "concats company and location with dash" do
-        helper.company_location_tag(vacancy).should == "#{vacancy.company} - #{vacancy.location}"
+RSpec.describe VacanciesHelper do
+  let(:vacancy) { build(:vacancy) }
+
+  subject { helper }
+
+  describe '#company_location_tag' do
+    context 'when company and location are presented' do
+      before do
+        vacancy.company = 'Monsters Inc.'
+        vacancy.location = 'Monstropolis'
+      end
+
+      it 'concats company and location with dash' do
+        expect(subject.company_location_tag(vacancy)).to eql(
+          "#{vacancy.company} - #{vacancy.location}"
+        )
       end
     end
-    context "when company is blank" do
-      before{ vacancy.company = nil }
-      
-      it "returns only location string" do
-        helper.company_location_tag(vacancy).should == vacancy.location
+
+    context 'when only company is blank' do
+      before do
+        vacancy.company = nil
+        vacancy.location = 'Monstropolis'
+      end
+
+      it 'returns only location string' do
+        expect(subject.company_location_tag(vacancy)).to eql(vacancy.location)
       end
     end
-    context "when location is blank" do
-      before{ vacancy.location = nil }
-      
-      it "returns only company string" do
-        helper.company_location_tag(vacancy).should == vacancy.company
+
+    context 'when only location is blank' do
+      before do
+        vacancy.company = 'Monsters Inc.'
+        vacancy.location = nil
+      end
+
+      it 'returns only company string' do
+        expect(subject.company_location_tag(vacancy)).to eql(vacancy.company)
       end
     end
-    context "when company and location are blank" do
+
+    context 'when company and location are blank' do
       before do
         vacancy.company = nil
         vacancy.location = nil
       end
-      
-      it "returns empty string" do
-        helper.company_location_tag(vacancy).should be_blank
+
+      it 'returns empty string' do
+        expect(subject.company_location_tag(vacancy)).to be_blank
       end
     end
   end

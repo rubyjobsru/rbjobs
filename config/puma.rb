471a3391394f421093ef0daccf533e8c33d1a75d
Alexander Sulim <hello@sul.im>
2016-08-06 00:13:48 +0200

    Rename Puma relatated environment vars

--- config/puma.rb
+++ config/puma.rb
@@ -4,8 +4,9 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum, this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
+min_threads_count = ENV.fetch("PUMA_MIN_THREADS") { 5 }.to_i
+max_threads_count = ENV.fetch("PUMA_MAX_THREADS") { 5 }.to_i
+threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
@@ -26,7 +27,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("PUMA_WORKERS") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code

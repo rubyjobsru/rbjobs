0eb7b2b78c197efbfb40b932cc10dc7df5917dc2
Alexander Sulim <hello@sul.im>
2016-08-11 14:16:44 +0200

    Remove unused code

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -8,16 +8,6 @@ class Vacancy < ActiveRecord::Base
   validates :email, :presence => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :unless => Proc.new { |vacancy| vacancy.email.blank? } }
   validates :expire_at, :presence => true
 
-  # before_create do |vacancy|
-  #   vacancy.owner_token = TokenGenerator.generate_token
-  #   vacancy.admin_token = TokenGenerator.generate_token
-  # end
-
-  # before_save do |vacancy|
-  #   vacancy.excerpt_html     = HtmlGenerator.render extract_excerpt(vacancy.description)
-  #   vacancy.description_html = HtmlGenerator.render vacancy.description
-  # end
-
   scope :approved, lambda { where("approved_at IS NOT NULL") }
   scope :not_approved, lambda { where({ :approved_at => nil }) }
   scope :not_outdated, lambda { |date| where("expire_at >= ?", date) }
@@ -35,11 +25,4 @@ class Vacancy < ActiveRecord::Base
   def expired?
     expire_at.past?
   end
-
-  # protected
-  #
-  # # Take the first three parts of text
-  # def extract_excerpt(text, divider = "\r\n\r\n")
-  #   text.lines(divider).to_a.each(&:strip!).reject(&:blank?).take(3).join(divider)
-  # end
 end

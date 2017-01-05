235967296927e5b17fac154751c1e7ab97569544
Alexander Sulim <hello@sul.im>
2017-01-05 20:46:55 +0100

    Tweak email initializer

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -9,21 +9,13 @@ if ENV['APPLICATION_URL'].present?
 end
 
 if ENV['MAIL_SERVER_URL'].present?
-  connection_url    = URI.parse(ENV['MAIL_SERVER_URL'])
-  connection_params = if connection_url.query.present?
-                        Hash[URI.decode_www_form(connection_url.query)]
-                      else
-                        {}
-                      end
+  connection_url = URI.parse(ENV['MAIL_SERVER_URL'])
 
   ActionMailer::Base.delivery_method = connection_url.scheme.to_sym
   ActionMailer::Base.smtp_settings   = {
     address:   connection_url.host,
     port:      connection_url.port,
     user_name: connection_url.user,
-    password:  connection_url.password,
-    domain:               connection_params['domain'],
-    authentication:       connection_params['authentication'],
-    enable_starttls_auto: connection_params['starttls_auto'] == 'true'
+    password:  connection_url.password
   }
 end

b69fc815bf540a90699ada164be5798214c18557
Alexander Sulim <hello@sul.im>
2018-01-05 12:15:40 +0100

    Switch back to static files serving

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,9 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'http://assets.rubyjobs.ru'
+  # config.action_controller.asset_host = 'http://assets.rubyjobs.ru'
+
+  # config.assets.prefix = '/'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
